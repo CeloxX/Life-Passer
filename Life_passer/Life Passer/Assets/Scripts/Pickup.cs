433f890f88626efeadc8defd56404fddc4898c47
CeloxX <lukasz.rygielski@gmail.com>
2021-05-24 20:08:18 +0200

    Add particles

--- Life_passer/Life Passer/Assets/Scripts/Pickup.cs
+++ Life_passer/Life Passer/Assets/Scripts/Pickup.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] GameObject coinPickupVFX;
     [SerializeField] int pointsForCoinPickup = 100;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameSession.Instance.AddToScore(pointsForCoinPickup);
         AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+        GameObject myVFX = Instantiate(coinPickupVFX, transform.position, transform.rotation);
+        Destroy(myVFX, 0.5f);
         Destroy(gameObject);
     }
 }
